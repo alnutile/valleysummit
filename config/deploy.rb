e8113b98a53d921d5e94270cec1fe2e60232814d
Alfred Nutile <alfrednutile@gmail.com>
2013-07-07 20:07:39 -0400

    Initialize

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/apachevhost"
+
+server "75.98.173.74", :web, :app, :db, primary: true
+
+set :user, "deploy"
+set :domain, "valleysummit.stagingarea.us"
+set :application, "valleysummit"
+set :deploy_to, "/var/www/#{application}/app"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:alnutile/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
